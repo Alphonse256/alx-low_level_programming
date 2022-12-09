1a901426ee0d4158bb3586d2fd8c7e408f8365ca
Alphonse256 <doctorfipaca241@gmail.com>
2022-12-08 19:23:02 -0800

    Printed the sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,7 +15,7 @@ long long int b;
 float z;
 	printf("Size of a char: %zu byte(s)\n", sizeof(x));
 	printf("Size of an int: %zu byte(s)\n", sizeof(y));
-	printf("Size of a long int: %zu bytes(s)\n", sizeof(m));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(m));
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(b));
 	printf("Size of a float: %zu byte(s)\n", sizeof(z));
 	return (0);
