318a0b8351c85a495676ce98f738883b3a80f36e
Alphonse256 <doctorfipaca241@gmail.com>
2022-12-08 19:19:12 -0800

    Printed the sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,13 +8,13 @@
 
 int main(void)
 {
-	char x;
-	int y;
-	long int m;
-	long long int b;
-	float z;
+char x;
+int y;
+long int m;
+long long int b;
+float z;
 	printf("Size of a char: %zu byte(s)\n", sizeof(x));
-	printf("Size of an in int: %zu byte(s)\n", sizeof(y));
+	printf("Size of an int: %zu byte(s)\n", sizeof(y));
 	printf("Size of a long int: %zu bytes(s)\n", sizeof(m));
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(b));
 	printf("Size of a float: %zu byte(s)\n", sizeof(z));
