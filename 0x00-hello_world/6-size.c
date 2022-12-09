16c71aba5b82581bcf3b2c3d7d7ebf58ca09cbf1
Alphonse256 <doctorfipaca241@gmail.com>
2022-12-08 19:11:52 -0800

    Printed the sizes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char x;
+	int y;
+	long int m;
+	long long int b;
+	float z;
+	printf("Size of a char: %zu byte(s)\n", sizeof(x));
+	printf("Size of an in int: %zu byte(s)\n", sizeof(y));
+	printf("Size of a long int: %zu bytes(s)\n", sizeof(m));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(b));
+	printf("Size of a float: %zu byte(s)\n", sizeof(z));
+	return (0);
+}
