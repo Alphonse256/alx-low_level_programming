53d2c5492bea99fbd617963ebd00920bedfb6bfd
Alphonse256 <doctorfipaca241@gmail.com>
2022-12-12 08:14:35 -0800

    Printed the combination on two digits

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int p = 0;
+
+	while (p < 16)
+	{
+		putchar(p + '0');
+		putchar(%x);
+		p++;
+	}
+
+	putchar('\n');
+	return (0);
+}
