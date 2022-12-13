0f54645a0669d5c5ea73cae36a524ef7df695060
Alphonse256 <doctorfipaca241@gmail.com>
2022-12-13 15:10:21 -0800

    Printed the character

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,9 @@
 #include "main.h"
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
+ * main - Entry point
  *
- * Return: On success 0
+ * Return: Always 0 (Success)
  */
 
 int main(void)
