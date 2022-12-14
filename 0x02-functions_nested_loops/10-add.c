3354625d6ee083aee7a5ccd009bd6f7b9ee709ce
Alphonse256 <doctorfipaca241@gmail.com>
2022-12-14 08:11:52 -0800

    Added two integers

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -4,13 +4,13 @@
  * add - Adding 2 integers
  * @r: First Parameter giving the output
  * @t: Second Parameter giving the output
- * Return: On success 0
+ * Return: On success 1.
  */
 
 int add(int r, int t)
 {
-	int sum = r + t;
+	int sum;
+	sum = r + t;
 
-	_putchar('0' + sum);
 	return (sum);
 }
