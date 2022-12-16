8e9ad7fca558f14f75a5eaf6d2fc3e237a4ddd6a
Alphonse256 <doctorfipaca241@gmail.com>
2022-12-16 10:07:37 -0800

    Printed the straight line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -10,16 +10,9 @@ void print_line(int n)
 {
 	int m;
 
-	if (n > 0)
-	{
-		for (m = 0; m <= n; m++)
+		for (m = 0; m < n; m++)
 		{
 			_putchar('_');
 		}
 		_putchar('\n');
-	}
-	else
-	{
-		_putchar('\n');
-	}
 }
