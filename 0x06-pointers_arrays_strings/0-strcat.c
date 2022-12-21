91f8a566bc51fc1f2ca67d1d7de388e01dcc9e81
Alphonse256 <doctorfipaca241@gmail.com>
2022-12-21 05:08:32 -0800

    Concatenated strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strcat - Function concatenating strings
+ * @dest: First string parameter
+ * @src: Second string parameter
+ * Return: On success 0
+ */
+
+char *_strcat(char *dest, char *src)
+{
+int a = -1, i;
+
+for (i = 0; dest[i] != '\0'; i++)
+;
+
+do {
+	a++;
+	dest[i] = src[a];
+	i++;
+}
+	while (src[a] != '\0');
+	return (dest);
+}
