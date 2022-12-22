75d017214682b6c26efc3d304406c335636c56ee
Alphonse256 <doctorfipaca241@gmail.com>
2022-12-22 14:05:37 -0800

    Copied a number of characters

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strncpy - function for copying characters
+ * @dest: First parameter to be copied to
+ * @src: Second parameter copied
+ * @n: Number of characters copied from src
+ * Return: On success dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
