8ee0f39c02bb72032bdb5b5fa424376b354b6097
Alphonse256 <doctorfipaca241@gmail.com>
2023-01-11 11:49:50 -0800

    Created an array of chars

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - Function creating and array of chars
+ * @size: The size of memory bytes to be allocated
+ * @c: The character to be stored
+ * Return: Pointer or NULL
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *ptr;
+
+	if (size == 0)
+	{
+		return ("NULL");
+	}
+
+	ptr = (char *)malloc(size * sizeof(c));
+
+	if (ptr == 0)
+	{
+		return ("NULL");
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			*(ptr + i) = c;
+		}
+	return (ptr);
+	}
+}
