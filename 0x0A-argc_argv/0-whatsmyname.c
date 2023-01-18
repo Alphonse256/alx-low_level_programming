c15efd51a1b532af06dfb99b350c8f064c5b86a0
Alphonse256 <doctorfipaca241@gmail.com>
2023-01-18 14:30:11 -0800

    Printed the name of the program

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints the name of the program
+ * @argc: array size
+ * @argv: count of array elements
+ * Return: always 0 Success
+ */
+
+int main(int argc, char **argv)
+{
+	puts(argv[argc - 1]);
+	return (0);
+}
