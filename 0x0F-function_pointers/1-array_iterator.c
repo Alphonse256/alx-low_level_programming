038b7a6d0b0ed82f0ea8ce89f653c161a4592993
Alphonse256 <doctorfipaca241@gmail.com>
2023-01-24 09:32:34 -0800

    Executed a function given as a parameter on each element of an array

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -13,12 +13,12 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 
 	size_t i;
 
-	if (array != NULL || size != NULL || action != NULL)
+	if (array == NULL || size == NULL || action == NULL)
 	{
-		for (i = 0; i < size; i++)
-		{
-			action(array[i]);
-		}
+		return;
 	}
-	return;
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
 }
