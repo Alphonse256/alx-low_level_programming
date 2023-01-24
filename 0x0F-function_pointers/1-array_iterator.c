fbb69e9f9e50c60c67c5adc9cbe920d080bfd211
Alphonse256 <doctorfipaca241@gmail.com>
2023-01-24 09:20:51 -0800

    Executed a function given as a parameter on each element of an array

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - function that prints name
+ * @array: array
+ * @size: size
+ * @action: action
+ **/
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+
+	size_t i;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
