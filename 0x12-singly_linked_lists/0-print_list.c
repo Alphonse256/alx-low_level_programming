86394d8f1b5345b462a6a18f3f0cf7394bff24ee
Alphonse256 <doctorfipaca241@gmail.com>
2023-02-03 22:02:31 -0800

    Bit manipulation

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stddef.h>
+
+/**
+ * print_list - Prints all the elements of a list_t list
+ * @h: The function parameter
+ * Return: Number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	if (str == NULL)
+	{
+		printf("[0] (nil)\n");
+	}
+
+	while (h)
+	{
+		printf("%lu\n", lists_t->str);
+	}
+}
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,2 @@
+Bit manipulation is the act of algorithmically manipulating bits or other pieces of data shorter than a byte.
+Bit fields are variables defined with a predefined width.
